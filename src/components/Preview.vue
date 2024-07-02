7fab7457b1f0a2614059e1e82d1e219e451194a5
GarriguesN <96322929+GarriguesN@users.noreply.github.com>
2024-07-02 13:36:02 +0200

    "Updated Preview.vue to use dynamic color styling from formData for various text elements."

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -24,23 +24,23 @@ const props = defineProps({
 
      <div class="p-5 w-[80%] h-[55%] text-center bg-[#282C34] border text-white rounded-b-lg font-mono">
         <div class="p-10 border-green-500 border-2 h-[70%]">
-            <h1 class="text-lg font-medium"><span class="font-medium text-green-500">{{formData.name.text ? formData.name.text : "name"}}</span> / <span class="font-medium text-gray-300">@{{formData.username.text ? formData.username.text : "username"}}</span></h1>
+            <h1 class="text-lg font-medium"><span class="font-medium" :style="{ color: formData.name.color }">{{formData.name.text ? formData.name.text : "name"}}</span> / <span class="font-medium" :style="{ color: formData.username.color }">@{{formData.username.text ? formData.username.text : "username"}}</span></h1>
             <ul class="mt-8 space-y-1">
                 <li>
-                    <p><strong class="mr-2">npm:</strong><span class="hover:cursor-pointer hover:underline">https://npmjs.com/<span class="text-red-500">~{{formData.npm.text ? formData.npm.text : "npmusername"}}</span></span></p>
+                    <p><strong class="mr-2">npm:</strong><span class="hover:cursor-pointer hover:underline">https://npmjs.com/<span :style="{ color: formData.npm.color }" >~{{formData.npm.text ? formData.npm.text : "npmusername"}}</span></span></p>
                 </li>
                 <li>
-                    <p><strong class="mr-2">GitHub:</strong><span class="hover:cursor-pointer hover:underline">https://github.com/<span class="text-green-400">{{formData.github.text ? formData.github.text : "githubtag"}}</span></span></p>
+                    <p><strong class="mr-2">GitHub:</strong><span class="hover:cursor-pointer hover:underline">https://github.com/<span :style="{ color: formData.github.color }">{{formData.github.text ? formData.github.text : "githubtag"}}</span></span></p>
                 </li>
                 <li>
-                    <p><strong class="mr-2">LinkedIn:</strong><span class="hover:cursor-pointer hover:underline">https://www.linkedin.com/in/<span class="text-blue-400">{{formData.linkedin.text ? formData.linkedin.text : "linkedinurl"}}</span></span></p>
+                    <p><strong class="mr-2">LinkedIn:</strong><span class="hover:cursor-pointer hover:underline">https://www.linkedin.com/in/<span :style="{ color: formData.linkedin.color }">{{formData.linkedin.text ? formData.linkedin.text : "linkedinurl"}}</span></span></p>
                 </li>
                 <li>
-                    <p><strong class="mr-2">Wakatime:</strong><span class="hover:cursor-pointer hover:underline">https://wakatime.com/<span class="text-gray-400">{{formData.wakatime.text ? formData.wakatime.text : "wakatimename"}}</span></span></p>
+                    <p><strong class="mr-2">Wakatime:</strong><span class="hover:cursor-pointer hover:underline">https://wakatime.com/<span :style="{ color: formData.wakatime.color }">{{formData.wakatime.text ? formData.wakatime.text : "wakatimename"}}</span></span></p>
                 </li>
             </ul>
 
-            <p class="mt-12"><strong class="m-2">Card:</strong><span class="text-red-600">npx</span><span class="ml-2 ">{{formData.package.text ? formData.package.text : "packagename"}}</span></p>
+            <p class="mt-12"><strong class="m-2">Card:</strong><span :style="{ color: formData.package.color }">npx</span><span class="ml-2 ">{{formData.package.text ? formData.package.text : "packagename"}}</span></p>
         </div>
 
         <div class="flex mt-8 ml-20">
